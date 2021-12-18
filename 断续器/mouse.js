af2f99ada36da00c599859fbcf9fcaca5c3e5793
Lx0815 <89496228+Lx0815@users.noreply.github.com>
2021-12-18 23:13:04 +0800

    Update mouse.js

--- 断续器/mouse.js
+++ 断续器/mouse.js
@@ -141,6 +141,20 @@
     init();
 })();
 
+
+/* 手机侧边栏默认不展开 */
+var mobile_sidebar_menus = document.getElementById("sidebar-menus");
+if (mobile_sidebar_menus) {
+    var menus_item_child = mobile_sidebar_menus.getElementsByClassName(
+        "menus_item_child"
+    );
+    var menus_expand = mobile_sidebar_menus.getElementsByClassName("expand");
+    for (var i = 0; i < menus_item_child.length; i++) {
+        menus_item_child[i].style.display = "none";
+    }
+}
+
+
 /*
 作者: Ethan.Tzy
 链接: https://ethant.top/articles/hexo541u/#%E9%BC%A0%E6%A0%87-%E5%AD%97%E8%B7%9F%E9%9A%8F%E6%A0%B7%E5%BC%8F
